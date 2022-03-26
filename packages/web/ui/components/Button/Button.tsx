fb2d8752f89b72e075c0c7fb8ff49a01c8cda9f4
Willian Ferreira <willianhf15@gmail.com>
2022-03-26 19:47:05 -0300

    fix(ui): button component minor issues

--- packages/web/ui/components/Button/Button.tsx
+++ packages/web/ui/components/Button/Button.tsx
@@ -2,32 +2,36 @@ import clsx from 'clsx';
 import { useMemo } from 'react';
 import { PolymorphicComponentProps } from '~/types/helpers';
 
-const Styling = {
+const styling = {
   base: 'text-base font-medium',
   colorSchemes: {
+    base: {
+      solid: null,
+      link: 'text-white hover:text-gray-200'
+    },
     blue: {
       solid: 'bg-blue-600 hover:bg-blue-700 text-white',
-      link: 'text-blue-600 hover:!text-blue-700'
+      link: 'text-blue-600 hover:text-blue-700'
     },
     red: {
       solid: 'bg-rose-600 hover:bg-rose-700 text-white',
-      link: 'text-rose-600 hover:!text-rose-700'
+      link: 'text-rose-600 hover:text-rose-700'
     }
   },
   variants: {
     solid: 'flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm',
-    link: 'whitespace-nowrap text-white hover:text-gray-200'
+    link: 'whitespace-nowrap'
   }
 };
 
 type Props =
   | {
       variant: 'link';
-      colorScheme?: keyof typeof Styling['colorSchemes'];
+      colorScheme?: keyof typeof styling['colorSchemes'];
     }
   | {
-      variant?: keyof typeof Styling['variants'];
-      colorScheme: keyof typeof Styling['colorSchemes'];
+      variant?: keyof typeof styling['variants'];
+      colorScheme: keyof typeof styling['colorSchemes'];
     };
 
 type ButtonProps<Component extends React.ElementType> = PolymorphicComponentProps<Component, Props>;
@@ -36,23 +40,21 @@ export function Button<Component extends React.ElementType = 'button'>({
   as,
   children,
   className,
-  colorScheme,
+  colorScheme = 'base',
   variant = 'solid',
   ...props
 }: ButtonProps<Component>) {
   const Component = as ?? 'button';
 
-  const computedClassNames = useMemo(() => {
-    const classNames = [Styling.base, className, Styling.variants[variant]];
-    if (colorScheme) {
-      classNames.push(Styling.colorSchemes[colorScheme][variant]);
-    }
-
-    return clsx(classNames);
-  }, [variant, className, colorScheme]);
+  const classes = clsx([
+    styling.base,
+    className,
+    styling.colorSchemes[colorScheme][variant],
+    styling.variants[variant]
+  ]);
 
   return (
-    <Component className={computedClassNames} {...props}>
+    <Component className={classes} {...props}>
       {children}
     </Component>
   );
