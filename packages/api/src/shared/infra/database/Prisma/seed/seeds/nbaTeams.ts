16f9f535e3ab174a5f4957ec50d92b75bbe7b059
Willian Ferreira <willianhf15@gmail.com>
2023-01-07 11:24:41 +0000

    fix(api): nbaTeams seed

--- packages/api/src/shared/infra/database/Prisma/seed/seeds/nbaTeams.ts
+++ packages/api/src/shared/infra/database/Prisma/seed/seeds/nbaTeams.ts
@@ -4,44 +4,40 @@ import { log } from '../log';
 import { prisma } from '../prisma';
 
 interface NBATeamsAPIResponse {
-  league: {
-    standard: Array<{
-      teamId: string;
-      fullName: string;
-      isNBAFranchise: boolean;
-      nickname: string;
-      tricode: string;
-      confName: 'East' | 'West';
-    }>;
-  };
+  data: {
+    id: number;
+    abbreviation: string;
+    city: string;
+    conference: string;
+    full_name: string;
+    name: string;
+  }[];
 }
 
 async function fetchNBATeams() {
-  return got('http://data.nba.net/10s/prod/v1/2021/teams.json').json<NBATeamsAPIResponse>();
+  return got('https://www.balldontlie.io/api/v1/teams').json<NBATeamsAPIResponse>();
 }
 
 export async function seedNBATeams() {
   log('Seeding NBA Teams...');
 
-  const data = await fetchNBATeams();
+  const response = await fetchNBATeams();
 
   await Promise.all(
-    data.league.standard
-      .filter(nbaTeam => nbaTeam.isNBAFranchise)
-      .map(nbaTeam => {
-        const team = {
-          id: nbaTeam.teamId,
-          name: nbaTeam.fullName,
-          nickname: nbaTeam.nickname,
-          tricode: nbaTeam.tricode,
-          conference: nbaTeam.confName === 'East' ? Conference.EAST : Conference.WEST
-        };
+    response.data.map(nbaTeam => {
+      const team = {
+        id: nbaTeam.abbreviation,
+        name: nbaTeam.full_name,
+        nickname: nbaTeam.name,
+        tricode: nbaTeam.abbreviation,
+        conference: nbaTeam.conference === 'East' ? Conference.EAST : Conference.WEST
+      };
 
-        prisma.nBATeam.upsert({
-          where: { id: team.id },
-          create: team,
-          update: team
-        });
-      })
+      return prisma.nBATeam.upsert({
+        where: { id: team.id },
+        create: team,
+        update: team
+      });
+    })
   );
 }
